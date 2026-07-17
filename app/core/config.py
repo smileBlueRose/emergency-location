b3b208294cb857c86d38e309cb4d95dd79a36807
Smile <156634569+smileBlueRose@users.noreply.github.com>
2026-07-17 22:42:09 +0500

    add more variables to DatabaseConfig

--- app/core/config.py
+++ app/core/config.py
@@ -1,24 +1,54 @@
 from pydantic import BaseModel, PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+PROJECT_DIR = Path(__file__).parent.parent.parent
+SECRETS_DIR = PROJECT_DIR / 'secrets'
+ENV_DIR = PROJECT_DIR / 'env'
 
 
 class RunConfig(BaseModel):
-    host: str = 'localhost'
-    port: int = 8000
+    host: str
+    port: int
     reload: bool = True
 
 
 class DatabaseConfig(BaseModel):
-    url: PostgresDsn
-    echo: bool = False
-    echo_pool: bool = False
-    pool_size: int = 5
-    max_overflow: int = 10
+    user: str
+    name: str
+    host: str
+    port: int
+    password_file: str
+
+    echo: bool
+    echo_pool: bool
+    pool_size: int
+    max_overflow: int
+
+    @property
+    def url(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=self.user,
+            password=self.get_password(),
+            host=self.host,
+            port=self.port,
+            path=self.name,
+
+        )
+
+    def get_password(self) -> str:
+        return Path(PROJECT_DIR / self.password_file).read_text()
 
 
 class Settings(BaseSettings):
-    run: RunConfig = RunConfig()
+    model_config = SettingsConfigDict(
+        env_file=(ENV_DIR / '.env.template', ENV_DIR / '.env'),
+        case_sensitive=False,
+        env_nested_delimiter='__',
+    )
+    run: RunConfig
     db: DatabaseConfig
 
 
-settings = Settings()
+settings = Settings()  # type: ignore
