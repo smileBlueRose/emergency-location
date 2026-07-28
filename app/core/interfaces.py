66bdad2f92b9ddf2d7aefbbc2761a13551332649
Smile <156634569+smileBlueRose@users.noreply.github.com>
2026-07-28 21:37:54 +0500

    remove AsyncClient from SmsGateway interface

--- app/core/interfaces.py
+++ app/core/interfaces.py
@@ -1,13 +1,10 @@
 from typing import Protocol
 from schemas.sms import SmsResult
-from httpx import AsyncClient
 from core.enums import SmsStatus
 from schemas.whatsapp import WhatsAppTemplate, WhatsAppSendResult
 
 
 class SmsGateway(Protocol):
-    _client: AsyncClient  # TODO: AsyncClient shouldn't be a part of interface
-
     async def send(self, phone: str, msg: str) -> SmsResult: ...
 
     async def get_status(self, msg_id: str, phone: str | None = None) -> SmsStatus: ...
