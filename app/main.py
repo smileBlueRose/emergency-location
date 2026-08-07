6891b8356696e9d89d05944f917dd5527f45121f
Smile <156634569+smileBlueRose@users.noreply.github.com>
2026-08-07 20:22:19 +0500

    mount MEDIA_ROOT to /media

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
 from api import router as api_router
-from core.config import settings
+from core.config import settings, MEDIA_ROOT
 from fastapi import FastAPI
 from core.log import configure_logger
 
 from core.exception_handler import register_exception_handlers
 from db import db_helper
 import uvicorn
+from fastapi.staticfiles import StaticFiles
 
 
 @asynccontextmanager
@@ -18,6 +19,8 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
 
 
 main_app = FastAPI(lifespan=lifespan)
+main_app.mount("/media", StaticFiles(directory=MEDIA_ROOT), name="media")
+
 main_app.include_router(api_router, prefix="/api")
 register_exception_handlers(main_app)
 configure_logger(main_app)
